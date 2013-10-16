233c31c446f38ff057cd8a470704c749e4bb4b7d
Estuardo Díaz <esjoando555@gmail.com>
2013-10-16 10:33:37 -0600

    prueba de codigo

--- buzzter/debug/clase.py
+++ buzzter/debug/clase.py
@@ -1,7 +1,8 @@
 __author__="t4r0"
 __date__ ="$14-oct-2013 16:53:32$"
 
-class pruebas():
+import unittest
+class pruebas:
     
     def pruebaIndice(self):
         print("Hola clase!")
@@ -18,5 +19,8 @@ class pruebas():
         try:
             for i in range(6):
                 print(arreglo[i])
-        except Exception:
+        except:
             print("Error, el indice esta fuera del rango")
+            
+class testcases(unittest.TestCase):
+    pass
--- buzzter/debug/prueba.py
+++ buzzter/debug/prueba.py
@@ -8,6 +8,6 @@ __date__ ="$15-oct-2013 18:06:25$"
 from clase import pruebas
 
 if __name__ == "__main__":
-    pruebas.pruebaIndice()
- 
+    prueba = pruebas()
+    prueba.errorCapturado()
     print "Hello World";
