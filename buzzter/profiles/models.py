47b752f7172c73077bc48a80b71cae1f89cbcf04
Estuardo Díaz <esjoando555@gmail.com>
2013-10-16 19:22:38 -0600

    Actualización-Se ha iniciado a trabajar el perfil del usuario

--- buzzter/profiles/models.py
+++ buzzter/profiles/models.py
@@ -2,8 +2,15 @@ from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
-class Perfil(User):
-    usuario = models.ForeingKey(User, unique=True)
+class Profile(models.Model):
+    """
+    Esta clase representa el modelo de los perfiles que se utilizarán
+    Fecha: 16/10/2013 19:14
+    Autor: T4r0_o
+    Branch: master
+    Modificado: 16/10/2013 19:14
+    """
+    usuario = models.ForeignKey(User, unique=True)
     nombre = models.CharField(max_length=300, blank=True)
     fotografia = models.CharField(max_length=500, blank=True)
     
