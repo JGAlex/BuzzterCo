18c197540bc9e7a2ebcf619d9e1d180b754e49bb
Estuardo Díaz <esjoando555@gmail.com>
2013-10-19 00:50:18 -0600

    Actualización-Estilos he empezado a trabajar con los estilos

--- buzzter/buzzter/settings.py
+++ buzzter/buzzter/settings.py
@@ -74,6 +74,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    PROJECT_PATH.child('templates'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
