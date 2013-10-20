dc80e467b1390af1bc1c31f74bd8ba1c01b0e40e
JGAlex <j.or.alq@gmail.com>
2013-10-20 17:28:43 -0600

    Nuevo - Características Following y Follower

--- buzzter/following/models.py
+++ buzzter/following/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from profiles.models import Profile
 
+class Follower(models.Model):
+    """
+    Clase para el manejo de los seguidores de un usuario
+    Date: 20-10-2013 16:26
+    Author: JGalex
+    Branch: JGBranch
+    """
+    
+    follower = models.ForeignKey(Profile)
+    usuario = models.ForeignKey(Profile, related_name = "seguidores")
+    
+    def __unicode__(self):
+        return self.follower.get_username()
+
+class Following(models.Model):
+    """
+    Clase para el manejo de los usuarios que se siguen
+    Date: 20-10-2013 17:15
+    Author: JGalex
+    Branch: JGBranch
+    """
+    
+    following = models.ForeignKey(Profile)
+    usuario = models.ForeignKey(Profile, related_name = "siguiendo")
+    
+    def __unicode__(self):
+        return self.following.get_username()
 # Create your models here.
