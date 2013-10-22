aef07ffa049256f29e0a3ff4ccd9f1499e3ebbb1
Estuardo Díaz <esjoando555@gmail.com>
2013-10-21 18:59:39 -0600

    Actualización-Perfil Se ha empezado a trabajar en la edición del perfil

--- buzzter/profiles/views.py
+++ buzzter/profiles/views.py
@@ -2,12 +2,14 @@
 
 from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render, render_to_response
+from django.template import RequestContext, loader
 from django.views.generic import FormView
 from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from profiles.forms import SignUpForm, EditUserForm, EditProfileForm
 from profiles.models import Profile
-from forms import EditForm
-from django.core.context_processors import csrf
+
 def ProfileView(request, user_name):    
     user = User()
     try:
@@ -15,22 +17,8 @@ def ProfileView(request, user_name):
     except User.DoesNotExist:
         raise Http404
     return render(request, 'profiles/profile.html', {'profile':user, 'info':user.profile })
-def EditView(request):
-    if request.POST:
-        form = EditForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect('/Edit')
-    else:
-        form = EditForm()
-    args = {}
-    args.update(csrf(request))
-    args['form']=form
-    return render_to_response('profiles/editProfile.html', args)
 
 class SignUp(FormView):
-    from profiles.forms import SignUpForm
-    from profiles.models import Profile
     template_name = "profiles/signUp.html"
     form_class = SignUpForm
     success_url = reverse_lazy('login')
@@ -40,3 +28,19 @@ class SignUp(FormView):
         perfil = Profile(usuario = user)
         perfil.save()
         return super(SignUp, self).form_valid(form)
+    
+@login_required
+def editProfile(request):
+    if request.method == POST:
+        userForm = EditUserForm(request.POST, instance=request.user)
+        profileForm = EditProfileForm(request.POST, instance=request.user.profile)
+        
+        if userForm.is_valid() and profileForm.is_valid():
+            userForm.save()
+            profileForm.save()
+            return HttpResponseRedirect('/Now/')
+    else:
+        userForm = EditUserForm(instance = request.user)
+        profileForm = EditProfileForm(instance = request.user.profile)
+    return render_to_response('profiles/editProfile.html',{'userForm':userForm, 'profileForm', profileForm})
+            
