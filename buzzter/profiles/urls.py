54ca8cd5a19d48b47501a55302e8fbcbab7e9be0
Estuardo Díaz <esjoando555@gmail.com>
2013-10-21 20:05:47 -0600

    Actualización-Estilos

--- buzzter/profiles/urls.py
+++ buzzter/profiles/urls.py
@@ -7,9 +7,6 @@ from django.conf.urls import patterns, url
 from profiles import views 
 from following import views as follow
 urlpatterns = patterns('', 
-                        url(r'^Login$', 'django.contrib.auth.views.login',
-                            {'template_name':'profiles/login.html'}, name = 'login'),
-                        url(r'^Logout/$', 'django.contrib.auth.views.logout_then_login', name = 'logout'),
                         url(r'^Accounts/(?P<user_name>\w+)/$', views.ProfileView, name='profile'),
                         url(r'^Edit/$', views.editProfile, name='edit_profile'),
                         url(r'^Following/$', follow.FollowingView, name='following'),
