9db4c5777e222e00374b238a5bd89f483e531104
Estuardo Díaz <esjoando555@gmail.com>
2013-10-21 18:29:54 -0600

    Actualización-Perfiles
    Edición de perfil

--- buzzter/profiles/forms.py
+++ buzzter/profiles/forms.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # To change this license header, choose License Headers in Project Properties.
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
@@ -58,4 +59,71 @@ class SignUpForm(forms.ModelForm):
     
     def hasErrors(self):
         return bool(self.errors)
+=======
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="t4r0"
+__date__ ="$18-oct-2013 18:49:08$"
+
+from django import forms 
+from django.contrib.auth.models import User
+from models import Profile
+class EditForm(forms.ModelForm):
+    class Meta:
+        model =Profile
+        fields = ['pais','fotografia']
+        
+class SignUpForm(forms.ModelForm):  
+    """
+    Representa un formulario para registrarse en el sitio
+    Fecha: 18/10/2013 19:47
+    Autor: T4r0_o
+    Branch: taro
+    Modificado: 18/10/2013 
+    """
+    errorMessages={
+        'duplicate_username': "Un usuario con ese nombre ya existe",
+    }
+    
+    username = forms.RegexField(label="Username", max_length=30,
+        regex=r'^[\w.@+-]+$',
+        help_text="Requerido. 30 caracteres o menos, letras, digitos y los carcateres "
+                      "@/./+/-/_ .",
+        error_messages={
+            'invalid': "puedes usar solo los caraceteres "
+                         "@/./+/-/_"})
+    password = forms.CharField(label="Password",
+        widget=forms.PasswordInput)
+    email = forms.EmailField()
+    
+    class Meta:
+        model = User
+        fields = ("username",)
+    
+    def clean_username(self):
+        # Since User.username is unique, this check is redundant,
+        # but it sets a nicer error message than the ORM. See #13147.
+        username = self.cleaned_data["username"]
+        try:
+            User._default_manager.get(username=username)
+        except User.DoesNotExist:
+            return username
+        raise forms.ValidationError(
+            self.errorMessages['duplicate_username'],
+            code='duplicate_username',
+        )
+        
+    def save(self, commit=True):
+        user = super(SignUpForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        user.email(self.cleaned_data["email"])
+        if commit:
+            user.save()
+        return user
+    
+    def hasErrors(self):
+        return bool(self.errors)
+>>>>>>> Actualización-Perfil
     
--- buzzter/profiles/views.py
+++ buzzter/profiles/views.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from django.http import Http404
-from django.shortcuts import render
-from django.views.generic import CreateView, FormView
+from django.http import Http404, HttpResponseRedirect
+from django.shortcuts import render, render_to_response
+from django.views.generic import FormView
 from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth.models import User
 from profiles.models import Profile
-
+from forms import EditForm
+from django.core.context_processors import csrf
 def ProfileView(request, user_name):    
     user = User()
     try:
@@ -14,12 +15,18 @@ def ProfileView(request, user_name):
     except User.DoesNotExist:
         raise Http404
     return render(request, 'profiles/profile.html', {'profile':user, 'info':user.profile })
-
-class EditProfile(CreateView):
-    
-    template_name = 'profiles/editProfile.html'
-    model = Profile
-    success_url = reverse_lazy('profile')
+def EditView(request):
+    if request.POST:
+        form = EditForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/Edit')
+    else:
+        form = EditForm()
+    args = {}
+    args.update(csrf(request))
+    args['form']=form
+    return render_to_response('profiles/editProfile.html', args)
 
 class SignUp(FormView):
     from profiles.forms import SignUpForm
