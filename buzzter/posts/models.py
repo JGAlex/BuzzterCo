7c0ac1da6074e8d73e0ab5d4dd09f7828227605a
krnmt <karenmaytz@hotmail.com>
2013-10-24 16:45:04 -0600

    Cambio - Modelos

--- buzzter/posts/models.py
+++ buzzter/posts/models.py
@@ -13,24 +13,10 @@ class PostType(models.Model):
     Fecha: 20/10/13 16:41
     Autor: Karen (:
     Branch: Branch_Posts
-    Modificado: 20/10/13
+    Modificado: 24/10/13
     """
     
     tipo = models.CharField(max_length=20,blank=False,unique=True)
-    campos = models.CommaSeparatedIntegerField(max_length=40, blank=False)
-    listaCampos = {"1":"titulo","2":"link","3":"descripcion","4":"linkImagen",
-    "5":"autor","6":"interprete","7":"album","8":"year","9":"director",
-    "10":"clasificacion","11":"dibujante","12":"temporada","13":"tipoGenero"}
-    
-    def getFields(self):
-        atrbts = []
-        for num in self.campos.split(","):
-            atrbts.append(self.listaCampos[num])
-        return atrbts
-    
-    def getFieldsList(self):
-        return self.listaCampos
-    
     def __unicode__(self):
         return self.tipo
 
@@ -61,28 +47,35 @@ class Post(models.Model):
   Fecha: 18/10/13 16:50
   Autor: Karen
   Branch: Karen (local)
-  Modificado: 21/10/13 17:03
+  Modificado: 24/10/13 17:03
   """   
+   
+  usuario = models.ForeignKey(Profile)
+  tipoPublicacion = models.ForeignKey(PostType)
+  tipoGenero = models.ForeignKey(Genre, related_name="tipo_Genero")
+  
+  rating = models.PositiveIntegerField(blank=True, null=True,default=0)
+  year = models.PositiveIntegerField(default=2013)
   
   titulo = models.CharField(max_length=50, blank=False, unique=True)
-  link = models.URLField(max_length=150)
-  descripcion = models.TextField(blank=False)
-  rating = models.PositiveIntegerField(blank=True, null=True,default=0)
-  eliminar = models.BooleanField(blank=True,default=False)
-  linkImagen = models.URLField(max_length=150,blank=False)
   autor = models.CharField(max_length=50,blank=True, null=True,default="")
   interprete = models.CharField(max_length=50,blank=True, null=True,default="")
   album = models.CharField(max_length=50,blank=True, null=True,default="")
-  year = models.PositiveIntegerField()
   director = models.CharField(max_length=50,blank=True, null=True,default="")
   clasificacion = models.CharField(max_length=50,blank=True,null=True,default="")
   dibujante = models.CharField(max_length=50,blank=True, null=True,default="")
   temporada = models.CharField(max_length=50,blank=True, null=True,default="")
   
+  link = models.URLField(max_length=150,blank=False,null=False)
+  linkImagen = models.URLField(max_length=150,blank=False,null=False)
+  descripcion = models.TextField(blank=False)
   
-  usuario = models.ForeignKey(Profile)
-  tipoPublicacion = models.ForeignKey(PostType)
-  tipoGenero = models.ForeignKey(Genre, related_name="tipo_Genero", blank=True, null=True)
+  def set_titulo(self,titulo):
+      self.titulo = titulo.replace(" ","-")
+      return self.titulo
+  
+  def get_titulo(self,):
+      return self.titulo.replace("-"," ")
   
   def __unicode__(self):
       return self.titulo
