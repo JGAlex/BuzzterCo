64277b92f4b199e86c89bdd8c3588eccd3804c5f
Estuardo Díaz <esjoando555@gmail.com>
2013-11-04 20:26:04 -0600

    Actualizacion-Se han agregado nuevos estilos a as paginas

--- buzzter/posts/views.py
+++ buzzter/posts/views.py
@@ -27,7 +27,7 @@ def PostView(request, title):
 @login_required
 def now(request):
     listpost = Post.objects.all()[:8]
-    return render(request,"posts/newPost.html",{"posts": listpost})
+    return render(request,"posts/now.html",{"posts": listpost})
 
 @login_required
 def newPostMusic(request):
@@ -38,7 +38,7 @@ def newPostMusic(request):
     if tipoForm.is_valid():
         tipoForm.save()
         return HttpResponseRedirect('/Now/')
-    return render(request,"posts/newPostForm.html",{'form':tipoForm})
+    return render(request,"posts/newPost.html",{'form':tipoForm})
 
 @login_required
 def newPostMovies(request):
@@ -49,7 +49,7 @@ def newPostMovies(request):
     if tipoForm.is_valid():
         tipoForm.save()
         return HttpResponseRedirect('/Now/')
-    return render(request,"posts/newPostForm.html",{'form':tipoForm})
+    return render(request,"posts/newPost.html",{'form':tipoForm})
 
 @login_required
 def newPostSeries(request):
@@ -60,7 +60,7 @@ def newPostSeries(request):
     if tipoForm.is_valid():
         tipoForm.save()
         return HttpResponseRedirect('/Now/')
-    return render(request,"posts/newPostForm.html",{'form':tipoForm})
+    return render(request,"posts/newPost.html",{'form':tipoForm})
 
 @login_required
 def newPostPosters(request):
@@ -71,7 +71,7 @@ def newPostPosters(request):
     if tipoForm.is_valid():
         tipoForm.save()
         return HttpResponseRedirect('/Now/')
-    return render(request,"posts/newPostForm.html",{'form':tipoForm})
+    return render(request,"posts/newPost.html",{'form':tipoForm})
 
 @login_required
 def newPostArtBooks(request):
@@ -82,4 +82,4 @@ def newPostArtBooks(request):
     if tipoForm.is_valid():
         tipoForm.save()
         return HttpResponseRedirect('/Now/')
-    return render(request,"posts/newPostForm.html",{'form':tipoForm})
+    return render(request,"posts/newPost.html",{'form':tipoForm})
