b41e62cef29ab2cf57175ec2d0cf67f5bd72213a
Estuardo Díaz <esjoando555@gmail.com>
2013-11-10 15:42:45 -0600

    Checkpoint-Se han resuelto algunos conflictos

--- buzzter/messages/views.py
+++ buzzter/messages/views.py
@@ -10,13 +10,16 @@ from django.contrib.auth.models import User
 
 @login_required
 def SendMessage(request, user_name):
-    emisor = request.user
-    receptor = User.objects.get(username=user_name)
-    mensaje = Messages(emisor=emisor.profile, receptor=receptor.profile)
-    mensajes = emisor.profile.enviados.filter(receptor=receptor).order_by('fecha')
-    form = formMessage(request.POST or None, instance=mensaje)
-    if form.is_valid():
-        form.save()
-        Http404ResponseRedirect()
-    return render(request, 'messages/send.html', {'form':form,'mensajes':mensajes})
+    try:
+        emisor = request.user
+        receptor = User.objects.get(username=user_name)
+        mensaje = Messages(emisor=emisor.profile, receptor=receptor.profile)
+        mensajes = emisor.profile.enviados.filter(receptor=receptor).order_by('fecha')
+        form = formMessage(request.POST or None, instance=mensaje)
+        if form.is_valid():
+            form.save()
+            HttpResponseRedirect('Messages/'+receptor.username)
+        return render(request, 'messages/send.html', {'form':form,'mensajes':mensajes})
+    except User.DoesNotExist:
+        raise Http404
 
