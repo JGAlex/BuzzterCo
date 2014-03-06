170dba4f97b7fc21f81f540973c87808df89da85
krnmt <karenmaytz@hotmail.com>
2014-03-06 09:25:37 -0600

    Cambios de variables
    Se cambio los nombre de las variables

--- buzzter/posts/resources.py
+++ buzzter/posts/resources.py
@@ -9,14 +9,14 @@ from posts.models import PostType, Post, Comments
 class PostTypeResource(ModelResource):
     class Meta:
         queryset = PostType.objects.all()
-        resource_name = 'PostTypeR'
+        resource_name = 'PostTypeResource'
 
 class PostResource(ModelResource):
     class Meta:
         queryset = Post.objects.all()
-        resource_name = 'PostR'
+        resource_name = 'PostResource'
 
 class CommentsResource(ModelResource):
     class Meta:
         queryset = Comments.objects.all()
-        resource_name = 'CommentsR'
+        resource_name = 'CommentsResource'
