20e0797f74b956e1bf37aac853627c4b807f2dcd
Estuardo Díaz <esjoando555@gmail.com>
2014-03-13 09:45:54 -0600

    Merge origin/API into API
    Conflicts:
    	buzzter/buzzter/urls.py
    	buzzter/posts/resources.py

--- buzzter/following/resources.py
+++ buzzter/following/resources.py
@@ -1,7 +1,3 @@
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
-
 __author__="Jorge Quijivix <<j.or.alq@gmail.com>>"
 __date__ ="$12/03/2014 11:45:22 PM$"
 
