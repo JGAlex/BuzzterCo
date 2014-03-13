1c594ddca581ffea3391ee76ba3754e5d6bcb373
[JGalex] <j.or.alq@gmail.com>
2014-03-12 23:55:26 -0600

    Recurso following
    Se han añadido los recursos de following

---
+++ buzzter/following/resources.py
@@ -0,0 +1,20 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="Jorge Quijivix <<j.or.alq@gmail.com>>"
+__date__ ="$12/03/2014 11:45:22 PM$"
+
+
+from tastypie.resources import ModelResource
+from following.models import Follower, Following
+
+class FollowerResource(ModelResource):
+    class Meta:
+        queryset = Follower.objects.all()
+        resource_name = 'FollowerResource'
+        
+class FollowerResource(ModelResource):
+	class Meta:
+		queryset = Following.objects.all()
+		resource_name = 'FollowingResource'
