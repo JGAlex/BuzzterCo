8410c7b92e42a057b9aa550850d1e8c7ca78e988
Estuardo Díaz <esjoando555@gmail.com>
2014-03-20 00:01:01 -0600

    Anidando recursos

--- buzzter/profiles/resources.py
+++ buzzter/profiles/resources.py
@@ -1,6 +1,6 @@
 from buzzter.authentication import OAuth20Authentication
 from tastypie.authorization import DjangoAuthorization
-from tastypie.resources import ModelResource
+from tastypie.resources import ModelResource, ObjectDoesNotExist, MultipleObjectsReturned
 from tastypie.utils import trailing_slash
 from tastypie import fields
 from django.conf.urls import url
@@ -9,8 +9,8 @@ from models import Profile
 from posts.resources import *
 
 class ProfileResource(ModelResource):
-    username = fields.CharField(readonly=True)
-    posts = fields.ToManyField('posts.resources.PostResource','posts')
+    username = fields.CharField(readonly=True, attribute='username', null = True)
+    posts = fields.ToManyField('posts.resources.PostResource','posts', null=True)
     class Meta:
         queryset = Profile.objects.all()
         resource_name = 'account'
@@ -18,28 +18,14 @@ class ProfileResource(ModelResource):
         allowed_methods = ['get', 'post', 'put', 'patch']
         include_resource_uri = False
         authorization = DjangoAuthorization()
-        authentication = OAuth20Authentication()
+        authentication = OAuth20Authentication()    
     
     def dehydrate_username(self, bundle):
         return bundle.obj.__unicode__()
     
     def prepend_urls(self):
-        return [
-            url(r"^(?P<resource_name>%s)/(?P<pk>\w[\w/-]*)/posts%s$" % (self._meta.resource_name, trailing_slash()), self.wrap_view('get_children'), name="api_get_children"),
-        ]
-    
-    def get_children(self, request, **kwargs):
-        try:
-            bundle = self.build_bundle(data={'pk': kwargs['pk']}, request=request)
-            obj = self.cached_obj_get(bundle=bundle, **self.remove_api_resource_names(kwargs))
-        except ObjectDoesNotExist:
-            return HttpGone()
-        except MultipleObjectsReturned:
-            return HttpMultipleChoices("More than one resource is found at this URI.")
-
-        child_resource = PostResource()
-        return child_resource.get_detail(request, parent_id=obj.pk)
- 
+        return[
+    url(r"^(?P<resource_name>%s)/(?P<username>\w+)/posts%s$" % (self._meta.resource_name, trailing_slash()), self.wrap_view('get_posts'), name="api_get_children"),]
 class UserResource(ModelResource):
     profile = fields.ToOneField(ProfileResource, 'profile', related_name='user', full=True)
     class Meta:
@@ -49,8 +35,21 @@ class UserResource(ModelResource):
         fields = ['date_joined','first_name','last_name','is_staff']       
         detail_uri_name = 'username'
         authorization = DjangoAuthorization()
-        authentication = OAuth20Authentication()
+        authentication = OAuth20Authentication() 
+            
+    def get_profile(self, request, **kwargs):
+        try:
+            bundle = self.build_bundle(data={'username': kwargs['username']}, request=request)
+            obj = self.cached_obj_get(bundle=bundle, **self.remove_api_resource_names(kwargs))
+        except ObjectDoesNotExist:
+            return HttpGone()
+        except MultipleObjectsReturned:
+            return HttpMultipleChoices("More than one resource is found at this URI.")
+
+        child_resource = ProfileResource()
+        return child_resource.get_detail(request, id=obj.pk)
         
     def prepend_urls(self):
         return [
-            url(r'^(?P<resource_name>%s)/(?P<username>\w+)/$' % self._meta.resource_name, self.wrap_view('dispatch_detail'), name='api_dispatch_detail'),        ]
+            url(r'^(?P<resource_name>%s)/(?P<username>\w+)/$' % self._meta.resource_name, self.wrap_view('dispatch_detail'), name='api_dispatch_detail'),
+            url(r"^(?P<resource_name>%s)/(?P<username>\w+)/profile%s$" % (self._meta.resource_name, trailing_slash()), self.wrap_view('get_profile'), name="api_get_children"),]
