6870ef2c096654e52b67b14793a68f059357ed09
Estuardo Díaz <esjoando555@gmail.com>
2014-04-25 20:55:35 -0600

    Conflictos arreglados

--- buzzter/profiles/resources.py
+++ buzzter/profiles/resources.py
@@ -2,27 +2,17 @@ from buzzter.authentication import OAuth20Authentication
 from tastypie.authorization import DjangoAuthorization
 from tastypie.resources import ModelResource, ObjectDoesNotExist, MultipleObjectsReturned
 from tastypie import fields
-<<<<<<< HEAD
 from tastypie.http import *
 from django.conf.urls import url
 from django.contrib.auth.models import User
 from posts.resources import *
-=======
-from django.conf.urls import url
-from django.contrib.auth.models import User
-from posts.resources import *
-import pdb
->>>>>>> 5ee531fa7c1d1d2ba5beb9a5a6c8a68b5932beaf
 
 class UserResource(ModelResource):
     picture = fields.CharField(readonly=True, attribute='picture', null = True) 
     country = fields.CharField(readonly=True, attribute='country', null = True)
     flag = fields.CharField(readonly=True, attribute='flag', null = True)
-<<<<<<< HEAD
     followers = fields.IntegerField(attribute='followers_count', null=True)
     following = fields.IntegerField(attribute='following_count', null=True)
-=======
->>>>>>> 5ee531fa7c1d1d2ba5beb9a5a6c8a68b5932beaf
     
     class Meta:
         queryset = User.objects.all()
@@ -31,17 +21,14 @@ class UserResource(ModelResource):
         fields = ['date_joined','first_name','last_name','is_staff', 'username']       
         detail_uri_name = 'username'
         authorization = DjangoAuthorization()
-        authentication = OAuth20Authentication() 
-<<<<<<< HEAD
+        authentication = OAuth20Authentication()
+        
     def dehydrate_followers(selfs,bundle):
         return bundle.obj.profile.followers.count()
     
     def dehydrate_following(selfs,bundle):
         return bundle.obj.profile.followings.count()
     
-=======
-        
->>>>>>> 5ee531fa7c1d1d2ba5beb9a5a6c8a68b5932beaf
     def dehydrate_picture(self, bundle):
         return '/media/' + str(bundle.obj.profile.fotografia)
     
@@ -66,17 +53,11 @@ class UserResource(ModelResource):
             bundle = res.build_bundle(obj=post, request = request)
             bundle = res.full_dehydrate(bundle)
             objects.append(bundle)
-<<<<<<< HEAD
-=======
-
->>>>>>> 5ee531fa7c1d1d2ba5beb9a5a6c8a68b5932beaf
         object_list = {   
             'objects':objects
         }
         res.log_throttled_access(request)
         return res.create_response(request, object_list)
-<<<<<<< HEAD
-=======
 
 
     def get_followers(self, request, **kwargs):
@@ -120,7 +101,6 @@ class UserResource(ModelResource):
         object_list = {'objects': objects}
         res.log_throttled_access(request)
         return res.create_response(request,object_list)
->>>>>>> 5ee531fa7c1d1d2ba5beb9a5a6c8a68b5932beaf
     
     
     def prepend_urls(self):
